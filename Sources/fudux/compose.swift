64a8179892ee6de213826c77a42c82acdcdd0a14
Thurman1776- <thurman.dev1776@gmail.com>
2020-11-26 18:53:51 +0100

    Run swiftformat

--- Sources/fudux/compose.swift
+++ Sources/fudux/compose.swift
@@ -19,6 +19,5 @@ infix operator >>>: CompositionPrecedence
 ///   - rhs: Second function which bridges intermediate value to end result
 /// - Returns: A function obtained by composing the argument functions
 func >>> <T, U, V>(lhs: @escaping (T) -> U, rhs: @escaping (U) -> V) -> (T) -> V {
-    return { initial in rhs(lhs(initial)) }
+    { initial in rhs(lhs(initial)) }
 }
-
--- Tests/fuduxTests/ComposeTests.swift
+++ Tests/fuduxTests/ComposeTests.swift
@@ -9,14 +9,13 @@
 import XCTest
 
 final class ComposeTests: XCTestCase {
-    
     override func setUpWithError() throws {
         try super.setUpWithError()
-        
+
         loggedActions = [:]
         orderedValues = []
     }
-    
+
     func test_function_composition_is_valid_for_arbitrary_types() {
         func firstFunc(_ value: Int) -> Int { Int(arc4random_uniform(UInt32(value))) }
         func secondFunc(_ number: Int) -> String {
@@ -24,23 +23,23 @@ final class ComposeTests: XCTestCase {
             formatter.numberStyle = .spellOut
             return formatter.string(from: number as NSNumber) ?? ""
         }
-        
+
         let sut = firstFunc >>> secondFunc
         let result = sut(10)
         XCTAssert(result.isEmpty == false, "Expected a non-empty string!")
     }
-    
+
     func test_enhancing_store() throws {
         let firstEnhancer: StoreEnhancer<ReduxAppState> = firstEnhancingFunction("first")
         let secondEnhancer: StoreEnhancer<ReduxAppState> = secondEnhancingFunction("second")
         let expectedState = ReduxAppState(title: "New title", isLoading: false, sideEffectResult: .idle)
-        
+
         let sut = firstEnhancer >>> secondEnhancer
         let enhancedStore = sut(createStore)
         let (dispatch, _, getState) = enhancedStore(reduxReducer, ReduxAppState.initialState)
-        
+
         dispatch(ReduxAction.setTitle("New title"))
-        
+
         XCTAssert(
             expectedState == getState(),
             "Expected states to match but found \(getState())"
@@ -62,23 +61,23 @@ final class ComposeTests: XCTestCase {
             "Expected to find value <second> but got \(orderedValues[1])"
         )
     }
-    
+
     func test_enhancing_store_with_middlewares_first() {
         let firstEnhancer: StoreEnhancer<ReduxAppState> = firstEnhancingFunction("one")
         let secondEnhancer: StoreEnhancer<ReduxAppState> = secondEnhancingFunction("two")
         let middleware = applyMiddleware(middlewares: [dummyMiddleware()])
         let expectedState = ReduxAppState(title: "New title", isLoading: false, sideEffectResult: .idle)
-        
+
         // Set the middleware first on the chain
         // Its job is to "clean" "loggedActions" & "orderedValues" BEFORE the enhancers are run
         let sut = middleware >>> firstEnhancer >>> secondEnhancer
         let enhancedStore = sut(createStore)
         let (dispatch, _, getState) = enhancedStore(reduxReducer, ReduxAppState.initialState)
-        loggedActions = ["fake":ReduxAction.isLoading(true)]
+        loggedActions = ["fake": ReduxAction.isLoading(true)]
         orderedValues = ["fake"]
 
         dispatch(ReduxAction.setTitle("New title"))
-        
+
         XCTAssert(
             expectedState == getState(),
             "Expected states to be \(expectedState) but found \(getState())"
@@ -104,23 +103,23 @@ final class ComposeTests: XCTestCase {
             "Expected to find value <two> but got \(orderedValues[1])"
         )
     }
-    
+
     func test_enhancing_store_with_middlewares_last() {
         let firstEnhancer: StoreEnhancer<ReduxAppState> = firstEnhancingFunction("one")
         let secondEnhancer: StoreEnhancer<ReduxAppState> = secondEnhancingFunction("two")
         let middleware = applyMiddleware(middlewares: [dummyMiddleware()])
         let expectedState = ReduxAppState(title: "New title", isLoading: false, sideEffectResult: .idle)
-        
+
         // Set the middleware last on the chain
         // Its job is to "clean" "loggedActions" & "orderedValues" AFTER the enhancers are run
         let sut = firstEnhancer >>> secondEnhancer >>> middleware
         let enhancedStore = sut(createStore)
         let (dispatch, _, getState) = enhancedStore(reduxReducer, ReduxAppState.initialState)
-        loggedActions = ["fake":ReduxAction.isLoading(true)]
+        loggedActions = ["fake": ReduxAction.isLoading(true)]
         orderedValues = ["fake"]
 
         dispatch(ReduxAction.setTitle("New title"))
-        
+
         XCTAssert(
             expectedState == getState(),
             "Expected states to be \(expectedState) but found \(getState())"
@@ -141,32 +140,32 @@ private var orderedValues: [String] = []
 
 // None of these enhancers swallow actions from original store's dispatch!!
 private func firstEnhancingFunction<State>(_ value: String) -> StoreEnhancer<State> {
-    return { createStore in {
+    { createStore in {
         reducer, state in
         let (storeDispatch, storeSubscribe, storeGetState) = createStore(reducer, state)
-        
+
         let loggingThunk: DispatchFunction = { action in
             storeDispatch(action)
             loggedActions[value] = action
             orderedValues.append(value)
         }
-        
+
         return (loggingThunk, storeSubscribe, storeGetState)
     }
     }
 }
 
 private func secondEnhancingFunction<State>(_ value: String) -> StoreEnhancer<State> {
-    return { createStore in {
+    { createStore in {
         reducer, state in
         let (storeDispatch, storeSubscribe, storeGetState) = createStore(reducer, state)
-        
+
         let loggingThunk: DispatchFunction = { action in
             storeDispatch(action)
             loggedActions[value] = action
             orderedValues.append(value)
         }
-        
+
         return (loggingThunk, storeSubscribe, storeGetState)
     }
     }
@@ -176,13 +175,13 @@ private func dummyMiddleware() -> Middleware<ReduxAppState> {
     { _, _ in { next in
         { action in
             next(action)
-            
+
             switch action as! ReduxAction {
-                case .setTitle:
-                    loggedActions = [:]
-                    orderedValues = []
-                default:
-                    break
+            case .setTitle:
+                loggedActions = [:]
+                orderedValues = []
+            default:
+                break
             }
         }
     }
